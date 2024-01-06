0294dacc2791aeca64850ff2e44e99827a1e8119
Nader <nadertate@gmail.com>
2024-01-06 06:24:21 +0200

    Update fetch-blog-posts.js

--- utils/fetch-blog-posts.js
+++ utils/fetch-blog-posts.js
@@ -53,7 +53,8 @@ const fetchPosts = async () => {
 };
 
 function generatePostCard(post) {
-  return `<a href="https://nailed-it.tech/articles/${post.slug.current}" target="_blank"><img src="${post.thumbnail}" width="400" /></a>`;
+  const key = {Math.random()*100}
+  return `<a key={${key}} href="https://nailed-it.tech/articles/${post.slug.current}" target="_blank"><img src="${post.thumbnail}" width="400" /></a>`;
 }
 async function generateReadme(posts) {
   try {
